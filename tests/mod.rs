c3000a4f7d89398e0ab0422b7d07a5f476b383b5
Wilberto Morales <wilbertomorales777@gmail.com>
2014-11-07 03:10:58 -0600

    Last sunk changes the token sink so now i need to compare only the tokens in the tests

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,13 +1,13 @@
 extern crate csspie;
 
-use csspie::tokenizer::{Token, TokenSink, tokenize};
+use csspie::tokenizer::{Token, tokenize};
 
 #[test]
 fn test_tokenizer_letters() {
-    let result = tokenize(String::from_str("abc"));
-    let expected = TokenSink::from_vec(vec![Token::letter('a'),
-                                            Token::letter('b'),
-                                            Token::letter('c')]);
+    let result = tokenize(String::from_str("abc")).tokens;
+    let expected = vec![Token::letter('a'),
+                        Token::letter('b'),
+                        Token::letter('c')];
 
     assert_eq!(result, expected);
 
@@ -15,8 +15,8 @@ fn test_tokenizer_letters() {
 
 #[test]
 fn test_tokenize_whitespace() {
-    let result = tokenize(String::from_str(" "));
-    let expected = TokenSink::from_vec(vec![Token::whitespace()]);
+    let result = tokenize(String::from_str(" ")).tokens;
+    let expected = vec![Token::whitespace()];
 
     assert_eq!(result, expected);
 
@@ -24,17 +24,17 @@ fn test_tokenize_whitespace() {
 
 #[test]
 fn test_tokenize_digits() {
-    let result = tokenize(String::from_str("0123456789"));
-    let expected = TokenSink::from_vec(vec![Token::digit('0'),
-                                            Token::digit('1'),
-                                            Token::digit('2'),
-                                            Token::digit('3'),
-                                            Token::digit('4'),
-                                            Token::digit('5'),
-                                            Token::digit('6'),
-                                            Token::digit('7'),
-                                            Token::digit('8'),
-                                            Token::digit('9')]);
+    let result = tokenize(String::from_str("0123456789")).tokens;
+    let expected = vec![Token::digit('0'),
+                        Token::digit('1'),
+                        Token::digit('2'),
+                        Token::digit('3'),
+                        Token::digit('4'),
+                        Token::digit('5'),
+                        Token::digit('6'),
+                        Token::digit('7'),
+                        Token::digit('8'),
+                        Token::digit('9')];
 
     assert_eq!(result, expected);
 
@@ -42,21 +42,21 @@ fn test_tokenize_digits() {
 
 #[test]
 fn test_tokenize_specials() {
-    let result = tokenize(String::from_str("*[]=~^$:.#>+()"));
-    let expected = TokenSink::from_vec(vec![Token::star(),
-                                            Token::opening_bracket(),
-                                            Token::closing_bracket(),
-                                            Token::equal(),
-                                            Token::tilda(),
-                                            Token::caret(),
-                                            Token::dollar(),
-                                            Token::colon(),
-                                            Token::dot(),
-                                            Token::hash_tag(),
-                                            Token::greater_than(),
-                                            Token::plus(),
-                                            Token::opening_paren(),
-                                            Token::closing_paren()]);
+    let result = tokenize(String::from_str("*[]=~^$:.#>+()")).tokens;
+    let expected = vec![Token::star(),
+                        Token::opening_bracket(),
+                        Token::closing_bracket(),
+                        Token::equal(),
+                        Token::tilda(),
+                        Token::caret(),
+                        Token::dollar(),
+                        Token::colon(),
+                        Token::dot(),
+                        Token::hash_tag(),
+                        Token::greater_than(),
+                        Token::plus(),
+                        Token::opening_paren(),
+                        Token::closing_paren()];
 
     assert_eq!(result, expected);
 
