87343ddc195c4a7758e70da7a82e7ccba15bd658
Wilberto Morales <wilbertomorales777@gmail.com>
2014-11-08 19:50:17 -0600

    Real world testing examples

--- tests/mod.rs
+++ tests/mod.rs
@@ -74,3 +74,75 @@ fn test_tokenize_specials() {
     assert_eq!(result, expected);
 
 }
+
+#[test]
+fn test_tokenize_examples() {
+
+    // testing with real world examples
+
+    let examples = vec![
+      String::from_str(".location a:first-child"),
+      String::from_str("a"),
+      String::from_str(".block a.current.trait"),
+      String::from_str("._5bsm ._50bm ._po-"),
+      String::from_str("#bootloader_WO4pn"),
+      String::from_str(".avatar-change:hover"),
+      String::from_str("ul li:nth-child(2)"),
+      String::from_str("span[lang~=\"en-us\"]")
+    ];
+
+    let results = vec![
+      vec![
+        Dot, Identifier(String::from_str("location")),
+        Whitespace, Identifier(String::from_str("a")),
+        Colon, Identifier(String::from_str("first")),
+        Hyphen, Identifier(String::from_str("child"))
+      ],
+
+      vec![Identifier(String::from_str("a"))],
+
+      vec![
+        Dot, Identifier(String::from_str("block")), Whitespace,
+        Identifier(String::from_str("a")), Dot,
+        Identifier(String::from_str("current")), Dot,
+        Identifier(String::from_str("trait"))
+      ],
+
+      vec![
+        Dot, Identifier(String::from_str("_5bsm")),
+        Whitespace, Dot, Identifier(String::from_str("_50bm")),
+        Whitespace, Dot, Identifier(String::from_str("_po")),
+        Hyphen
+      ],
+
+      vec![
+        HashTag, Identifier(String::from_str("bootloader_WO4pn"))
+      ],
+
+      vec![
+        Dot, Identifier(String::from_str("avatar")), Hyphen,
+        Identifier(String::from_str("change")), Colon,
+        Identifier(String::from_str("hover"))
+      ],
+
+      vec![
+        Identifier(String::from_str("ul")), Whitespace,
+        Identifier(String::from_str("li")),
+        Colon, Identifier(String::from_str("nth")), Hyphen,
+        Identifier(String::from_str("child")), OpeningParen,
+        Integer(String::from_str("2")), ClosingParen
+      ],
+
+      vec![
+        Identifier(String::from_str("span")), OpeningBracket,
+        Identifier(String::from_str("lang")), Tilda, Equal,
+        Quote, Identifier(String::from_str("en")), Hyphen,
+        Identifier(String::from_str("us")), Quote, ClosingBracket
+      ]
+
+    ];
+
+    for (example, result) in examples.iter().zip(results.iter()) {
+        assert_eq!(tokenize(example.clone()).tokens, *result);
+    }
+}
